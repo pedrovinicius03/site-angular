e4d0e410b85b0e2158dc2ab45bd711bdf7b53bfb
Pedro Vinicius <pedrovinicius883@gmail.com>
2022-10-13 16:04:30 -0300

    site angular

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 // import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from '../views/home/home.component';
+import { LoginComponent } from "../views/login/login.component";
 
 const routes: Routes = [
 
-  {path : "", component: HomeComponent}
+  {path : "", component: HomeComponent},
+  {path: "login", component: LoginComponent}
+
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from "./app-routing/app-routing.module";
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './views/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
